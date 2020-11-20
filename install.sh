0b3c9efe0eb94bfc8d2ecd82879a34dc510acffe
Marcelo Maia <mmaia.cc@gmail.com>
2020-11-19 22:40:38 -0300

    feat: added an uninstall script

--- install.sh
+++ install.sh
@@ -1,28 +1,46 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${RED}
+ ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓
+ ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒
+▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░
+▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██
+▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒
+░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░
+░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░
+   ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░
+         ░    ░  ░    ░ ░        ░   ░         ░
+${NC}"
+
 mkdir -p ~/.config/nvim/after
 mkdir -p ~/.config/nvim/vscode-snippets
 mkdir -p ~/.config/coc/ultisnips/
 mkdir -p ~/.omnisharp
 
-echo "Copying init.vim..."
+echo -e "${GREEN} Copying init.vim...${NC}"
 cp ./vim/init.vim ~/.config/nvim/init.vim
 
-echo "Copying language specific files..."
+echo -e "${GREEN} Copying language specific files...${NC}"
 cp -r ./ftplugin ~/.config/nvim/after/
 
-echo "Copying Utilsnips snippets..."
+echo -e "${GREEN} Copying Utilsnips snippets...${NC}"
 cp -r ./snippets/*.snippets ~/.config/coc/ultisnips/
 
-echo "Copying Vscode snippets..."
+echo -e "${GREEN} Copying Vscode snippets...${NC}"
 cp -r ./snippets/*.json ~/.config/nvim/vscode-snippets
 
-echo "Copying Coc settings..."
+echo -e "${GREEN} Copying Coc settings...${NC}"
 cp ./settings/coc-settings.json ~/.config/nvim/coc-settings.json
 
-echo "Copying Omnisharp settings..."
+echo -e "${GREEN} Copying Omnisharp settings...${NC}"
 cp ./settings/omnisharp.json ~/.omnisharp
 
 
-echo "Copying fancy fonts"
+echo -e "${GREEN} Copying fancy fonts${NC}"
 [[ ! -d "$HOME/.config/nvim/figlet/fonts" ]] && mkdir -p ~/.config/nvim/figlet/fonts && git clone https://github.com/xero/figlet-fonts ~/.config/nvim/figlet/fonts
+
+[[ ! -d "$HOME/.vim/bundle/markdown-preview.nvim/node_modules" ]] && cd ~/.vim/bundle/markdown-preview.nvim && yarn install && cd -
